8fbecbc3cd88bab1c99001d933f85eba771dd5a8
Jason Franklin <jason.franklin@xing.com>
2016-09-11 13:53:30 +0200

    Draw eyes in the face view

--- FaceIt/FaceView.swift
+++ FaceIt/FaceView.swift
@@ -10,14 +10,56 @@ import UIKit
 
 class FaceView: UIView {
 
+    let scale = CGFloat(0.8)
+
     override func drawRect(rect: CGRect) {
-        let faceRadius = min(bounds.size.width, bounds.size.height) / 2
-        let faceCenter = CGPoint(x: bounds.midX, y: bounds.midY)
+        pathForCenter(faceCenter, withRadius: faceRadius)
+            .stroke()
+        pathForEye(.Left).stroke()
+        pathForEye(.Right).stroke()
+    }
 
-        let path = UIBezierPath(arcCenter: faceCenter, radius: faceRadius, startAngle: 0.0, endAngle: CGFloat(2*M_PI), clockwise: false)
+    // private
+    private var faceRadius: CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2 * scale
+    }
 
+    private var faceCenter: CGPoint {
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+
+    private func pathForCenter(center: CGPoint, withRadius radius: CGFloat) -> UIBezierPath {
+        let path = UIBezierPath(arcCenter: center,
+                            radius: radius,
+                            startAngle: 0.0,
+                            endAngle: CGFloat(2*M_PI),
+                            clockwise: false)
         UIColor.blueColor().set()
         path.lineWidth = 5.0
-        path.stroke()
+        return path
+    }
+
+    private func pathForEye(eye: Eye) -> UIBezierPath {
+        let eyeRadius = faceRadius / Ratios.EyeToFaceRadiusScale
+        var eyeCenter = faceCenter
+
+        eyeCenter.y -= faceRadius / Ratios.EyeToFaceCenterOffset
+
+        switch(eye) {
+        case .Left: eyeCenter.x -= faceRadius / Ratios.EyeToFaceCenterOffset
+        case .Right: eyeCenter.x += faceRadius / Ratios.EyeToFaceCenterOffset
+        }
+
+        return pathForCenter(eyeCenter, withRadius: eyeRadius)
+    }
+
+    private enum Eye {
+        case Left
+        case Right
+    }
+
+    private struct Ratios {
+        static let EyeToFaceCenterOffset = CGFloat(3.0)
+        static let EyeToFaceRadiusScale = CGFloat(10.0)
     }
 }
